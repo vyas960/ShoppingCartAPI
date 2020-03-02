80fb2fed4337c617ae8e3719311acc183c5aab5e
vyas960 <ashutosh.webllisto@gmail.com>
2020-03-02 21:04:40 +0530

    2/march/2020

--- shopApp/views.py
+++ shopApp/views.py
@@ -15,6 +15,10 @@ from django.http import HttpResponse
 from allauth.account.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from shoppingCartApi.settings import EMAIL_HOST_USER
+#from django.core.mail import send_mail
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
 # from rest_framework import permissions
 # from shopApp.permissions import IsOwnerOrReadOnly
 # from rest_framework.permissions import IsAuthenticated
@@ -51,10 +55,27 @@ class ProductCreateView(generics.CreateAPIView):
 	# permission_classes = (IsAuthenticated,)
 	queryset = Product.objects.all()
 	serializer_class = ProductCreateSerializer
+	# subject = 'Welcome to ShoppingCart App'
+	# message = 'Product created successfully'
+	# recepient ='ashutosh.webllisto@gmail.com'
+	# send_mail(subject,message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+
+	# def send_email(request):
+	# 	subject = 'Welcome to ShoppingCart App'
+	# 	message = 'Product created successfully'
+	# 	recepient = self.request.user.email
+	# 	from_email = 'pythondjango97@gmail.com'
+	# 	send_mail(subject, message, from_email, [recepient], fail_silently = False)
+	# 	print("successfully sent mail")
 
 	def perform_create(self, serializer):
 	   serializer.save(owner=self.request.user)
-
+	   message = 'Product created successfully'
+	   subject = 'Welcome to ShoppingCart App'
+	   recepient = self.request.user.email
+	   from_email = 'pythondjango97@gmail.com'
+	   send_mail(subject, message, from_email, [recepient], fail_silently = False)
+	   print("Product created successfully")
 
 
 @method_decorator(csrf_exempt,name='dispatch')
@@ -98,3 +119,30 @@ class CustomerDetail(generics.ListAPIView):
 # class ProductWithUser(generics.RetrieveAPIView):
 #     queryset = Product.objects.all()
 #     serializer_class = ProductWithUserSerializers
+
+
+# 	from django.core.mail import send_mail
+# 	from blog.forms import EmailSendForm
+# 	def mail_send_view(request,id):
+# 	    post=get_object_or_404(Post,id=id,status='published')
+# 	    sent=False
+# 	    if request.method=='POST':
+# 	        form=EmailSendForm(request.POST)
+# 	        if form.is_valid():
+# 	            cd=form.cleaned_data
+# 	            subject='{}({}) recommends you to read "{}"'.format(cd['name'],cd['email'],post.title)
+# 	            post_url=request.build_absolute_uri(post.get_absolute_url())
+# 	            message='Read Post At:\n {}\n\n{}\'s Comments:\n{}'.format(post_url,cd['name'],cd['comments'])
+# 	            send_mail(subject,message,'durga@blog.com',[cd['to']])
+# 	            sent=True
+# 	    else:
+# 	        form=EmailSendForm()
+# 	    return render(request,'blog/sharebymail.html',{'form':form,'post':post,'sent':sent})
+#
+#
+# from django import forms
+# class EmailSendForm(forms.Form):
+#     name=forms.CharField()
+#     email=forms.EmailField()
+#     to=forms.EmailField()
+#     comments=forms.CharField(required=False,widget=forms.Textarea)
--- shoppingCartApi/settings.py
+++ shoppingCartApi/settings.py
@@ -167,4 +167,15 @@ LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 SIGNUP_REDIRECT_URL='/'
 
+#SMTP MAIL CONFIGURATION...
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='pythondjango97@gmail.com'
+EMAIL_HOST_PASSWORD='python0102030405'
+EMAIL_USE_TLS=True
+
+
+
+
 # https://briancaffey.github.io/2017/07/22/posting-json-data-with-ajax-to-django-rest-framework.html
